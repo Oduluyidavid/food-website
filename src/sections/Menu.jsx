d60e344547df6ae2f401d07cdbdd553489d9ea44
unknown <133164451+Oduluyidavid@users.noreply.github.com>
2024-03-15 21:48:02 +0100

    first commit

--- src/sections/Menu.jsx
+++ src/sections/Menu.jsx
@@ -39,11 +39,11 @@ export const Menu = () => {
       </section>
       <section id='about' className='flex flex-col lg:flex-row justify-center items-center gap-10 lg:py-24 py-20 px-10 bg-slate-950 w-full h-fit'>
         <div id='left' className='lg:w-[50%] w-full justify-end items-center lg:items-start gap-11'>
-          <h1 data-aos="zoom-in" className='text-white text-6xl font-bold'>About <span className='text-green-500 italic'>Us</span></h1>
-          <h1 className='text-white text-2xl font-semibold'>GRAB THIS TASTY FOOD</h1>
-          <p data-aos="zoom-in" className='text-white text-xl font-semibold text-center lg:text-start'>There's something irresistibly delightful about refreshment. From decadent hot burger to luscious burger cakes, 
+          <h1 data-aos="zoom-in" className='text-white text-6xl text-center font-bold'>About <span className='text-green-500 italic'>Us</span></h1>
+          <h1 className='text-white text-2xl text-center font-semibold'>GRAB THIS TASTY FOOD</h1>
+          <p data-aos="zoom-in" className='text-white text-xl font-semibold p-5 text-center lg:text-start'>There's something irresistibly delightful about refreshment. From decadent hot burger to luscious burger cakes, 
            every bite is a moment of pure bliss.</p>
-          <p data-aos="zoom-in" className='text-white text-xl font-semibold text-center lg:text-start'>We have curated a delactable collection of hot burgers and captions that will not only satisfy your craving but also add a sprinkle of charm and sweetness to what you order from us!.</p>
+          <p data-aos="zoom-in" className='text-white text-xl font-semibold p-5 text-center lg:text-start'>We have curated a delactable collection of hot burgers and captions that will not only satisfy your craving but also add a sprinkle of charm and sweetness to what you order from us!.</p>
           <button data-aos="zoom-in-up" data-aos-delay="400" className='bg-orange-500 px-8 py-4 rounded-full hover:bg-green-500 hover:text-black font-bold m-6'>ORDER NOW</button>
         </div>
         <div id='about-image' data-aos="slide-up" data-aos-delay="200" className='flex justify-center items-center lg:w-[50%] w-full'>
